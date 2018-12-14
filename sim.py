614ef5f7b9aea64118a0efff918b93c9555c2f05
Philip Nielsen <s174274@student.dtu.dk>
2018-12-14 19:51:28 +0100

    ann rect + conf + support

--- sim.py
+++ sim.py
@@ -10,6 +10,10 @@ TN = 3
 # split into K-datasplits where in each split 1/K is hold
 # out for testing and (K-1)/K is used for training.
 
+# p infinity norm = square
+# p 2 norm = circle
+# p 1 norm = diamond
+
 
 # computes f00
 def f00(a, b):
@@ -97,6 +101,54 @@ def gauss_kernel_density(x, M, var):
     return sum / len(x)
 
 
+# support for item set. x = list of lists with binarized data
+# indx_list = indexes of the item sets which are to be calculated
+def support(x, indx_list):
+    count = 0
+    for obs in x:
+        inner_count = 0
+        for indx in indx_list:
+            if obs[indx] == 1:
+                inner_count += 1
+        if inner_count == len(indx_list):
+            count += 1
+    return count / len(x)
+
+
+# calculates support for each item set included in indx_list
+def support_mult(x, indx_list):
+    supp_list = []
+    for indx in indx_list:
+        supp_list.append(support(x, indx))
+    return supp_list
+
+
+# confidence of association rule.
+# x = list of lists with binarized data
+# association rule = X --> Y
+# a_left = X, a_right = Y
+def conf(x, a_left, a_right):
+    top = support(x, a_left + a_right)
+    bot = support(x, a_left)
+    return top / bot
+
+
+# gmm probability, 1d.
+# x = observation to be predicted, wlist = list of weights, mlist = list of mu
+# vlist = list of var, K = the cluster x should be predicted in
+def gmm_1d(x, wlist, mlist, vlist, K):
+    sum = 0
+    pre = 0
+    ls = []
+    for w, mu, var in zip(wlist, mlist, vlist):
+        frac = 1 / math.sqrt(2 * math.pi * var)
+        ex = math.exp(-1 / (2 * var) * ((x - mu) ** 2))
+        pre = w * frac * ex
+        sum += pre
+        ls.append(pre)
+    return ls[K - 1] / sum
+
+
 # for assignment with scalar invariant etc..
 def sim_test(a, b):
     dot_sum = 0
@@ -106,6 +158,49 @@ def sim_test(a, b):
     dot_sum = dot_sum ** 2
     top1 = (euclid_norm(a) ** 2) * (euclid_norm(b) ** 2)
     return math.sqrt((top1 - dot_sum) / top1)
+
+
+# creates a 2d list containing the euclidean distances to each point
+def euclid_table_1d(x, inc_zero):
+    output = []
+    for calc_x in x:
+        temp = []
+        for val in x:
+            el = abs(calc_x - val)
+            if inc_zero:
+                temp.append(el)
+            elif el != 0:
+                temp.append(el)
+        output.append(temp)
+    return output
+
+
+def print_euclid_1d(x, inc_zero):
+    y = euclid_table_1d(x, inc_zero)
+    output = ''
+    top = '  '
+    for indx, v1 in enumerate(y):
+        top += '     ' + str(indx)
+        output += str(indx) + ' '
+        for v2 in v1:
+            output += '   ' + str(round(abs(v2), 2))
+        output += '\n'
+    top += '\n'
+    output = top + output
+    print(output)
+
+
+# ANN model with rectified linear function.
+# x = obs, h_units = list of h. units, w0 = weight 0.
+def ann_rect(x, h_units, w0):
+    sum = 0
+    len_diff = len(h_units[0]) - len(x)
+    w_dot = []
+    if (len_diff > 0):
+        w_dot = [1] * len_diff
+    for indx, w in enumerate(h_units):
+        sum += w0[indx + 1] * max(dot_product(w_dot + x, w), 0)
+    return w0[0] + sum
 
 
 # Evaluating classifier
@@ -124,16 +219,3 @@ def acc(x):
 
 def er(x):
     return 1 - acc(x)
-
-
-# Item set support and confidence
-# x is number of observations containing x
-# n is total number of observations
-def support(x, n):
-    return x / n
-
-# xy is number of observations that both include x and y
-# x is number of observations including x
-# n is total number of observations 
-def confidence(xy, x, n):
-    return support(xy,n) / support(x, n)
