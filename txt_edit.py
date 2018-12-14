02b9fa2ce8316b6ca7750b1f7a6250a61bf1afd8
Philip Nielsen <s174274@student.dtu.dk>
2018-12-14 12:24:56 +0100

    command line argument

--- txt_edit.py
+++ txt_edit.py
@@ -1,5 +1,7 @@
 import re
+import sys
 with open('parse_text.txt') as file:
+    inc_zero = sys.argv[1] == '1'
     output = open('output.txt', 'w+')
     output_string = ''
 
@@ -9,9 +11,9 @@ with open('parse_text.txt') as file:
         for indx, word in enumerate(line.split()):
             if re.search('[a-zA-Z]', word):
                 var_name = word + ' = '
-            elif indx == len(line.split()) - 1 and not word == '0':
+            elif indx == len(line.split()) - 1 and (not word == '0' or inc_zero):
                 write_line += word
-            elif not word == '0':
+            elif not word == '0' or inc_zero:
                 write_line += word + ', '
 
         write_line = var_name + write_line + ']' + '\n'
